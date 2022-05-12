999a991943b6774dd12c649bdde08482367c0879
noorak <nae.kataja@gmail.com>
2022-05-12 18:34:34 +0300

    modified CreateResources.py to accept templates folder

--- CreateResources.py
+++ CreateResources.py
@@ -2,14 +2,13 @@ import os
 
 SubscriptionId = '07c29cd9-f0b0-42f7-be1c-01d2cfc5ee2c'
 Location = 'northeurope'
-MyResourceGroup = 'testRG2'
+ResourceGroup = 'testRG'
 TemplateName = 'blanktemplate'
-TemplateFileName = 'azuredeploy.json'
-ParameterFile = '@vm_parameters.json'
+TemplateFile = 'templates/azuredeploy.json'
+ParameterFile = '@templates/vm_parameters.json'
 
 os.system('az login')
 os.system(f'az account set --subscription {SubscriptionId}')
-os.system(f'az group create --name {MyResourceGroup} --location {Location}')
-os.system(f'az group create --name {MyResourceGroup} --location {Location}')
-os.system(f'templateFile=".\{TemplateFileName}"')
-os.system(f'az deployment group create --name {TemplateName} --resource-group {MyResourceGroup} --template-file {TemplateFileName} --parameters {ParameterFile}')
+os.system(f'az group create --name {ResourceGroup} --location {Location}')
+os.system(f'az group create --name {ResourceGroup} --location {Location}')
+os.system(f'az deployment group create --name {TemplateName} --resource-group {ResourceGroup} --template-file {TemplateFile} --parameters {ParameterFile}')
