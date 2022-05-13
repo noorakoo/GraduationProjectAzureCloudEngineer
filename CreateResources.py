bb3c23e6d3a961724d58a59fb9bbd8459c48abbf
noorak <nae.kataja@gmail.com>
2022-05-13 11:35:40 +0300

    made ResizeVM.py and updated azure-pipelines.yml jobs

--- CreateResources.py
+++ CreateResources.py
@@ -2,7 +2,7 @@ import os
 
 SubscriptionId = '07c29cd9-f0b0-42f7-be1c-01d2cfc5ee2c'
 Location = 'northeurope'
-ResourceGroup = 'testRG'
+ResourceGroup = 'testRG3'
 TemplateName = 'blanktemplate'
 TemplateFile = 'templates/azuredeploy.json'
 ParameterFile = '@templates/vm_parameters.json'
