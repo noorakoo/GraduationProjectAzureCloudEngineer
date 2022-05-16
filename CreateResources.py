248019f5d6099f5f1e380d1445a30d52570c17a9
jcric <jonas.c.richter@me.com>
2022-05-16 08:54:52 +0200

    test

--- CreateResources.py
+++ CreateResources.py
@@ -1,7 +1,7 @@
 import os
 
 # Signing in and setting the subscription
-SubscriptionId = 'e4305bfa-9413-4c0e-8527-8f5307a80a27'
+SubscriptionId = 'e4305bfa-9413-4c0e-8527-8f5307a80a27' #academys
 Location = 'northeurope'
 os.system('az login')
 os.system(f'az account set --subscription {SubscriptionId}')
