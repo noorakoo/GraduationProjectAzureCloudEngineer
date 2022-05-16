84343006e3dcbcd08ddefff69be663c63d49ab34
jcric <jonas.c.richter@me.com>
2022-05-16 08:50:37 +0200

    Basic test and dev env

--- CreateResources.py
+++ CreateResources.py
@@ -10,27 +10,32 @@ os.system(f'az account set --subscription {SubscriptionId}')
 ResourceGroup = 'TestingRG'
 TemplateName = 'TestTemplate'
 os.system(f'az group create --name {ResourceGroup} --location {Location}')
-
+ 
 # Deploying the test VM
 TemplateFile = 'TestEnvTemplates/DeployTestVM.json'
 ParameterFile = '@TestEnvTemplates/TestVMParameters.json'
 os.system(f'az deployment group create --name {TemplateName} --resource-group {ResourceGroup} --template-file {TemplateFile} --parameters {ParameterFile}')
+
 
 # Deploying the test database
 TemplateFile = 'TestEnvTemplates/DeployTestDB.json'
 ParameterFile = '@TestEnvTemplates/TestDBParameters.json'
 os.system(f'az deployment group create --name {TemplateName} --resource-group {ResourceGroup} --template-file {TemplateFile} --parameters {ParameterFile}')
 
+# Creating the production environment
+ResourceGroup = 'ProductionRG'
+TemplateName = 'ProductionTemplate'
+os.system(f'az group create --name {ResourceGroup} --location {Location}')
+
+# Deploying two production VMs
+TemplateFile = 'ProdEnvTemplates/DeployProdVM.json'
+ParameterFile = '@ProdEnvTemplates/ProdVM1Parameters.json'
+os.system(f'az deployment group create --name {TemplateName} --resource-group {ResourceGroup} --template-file {TemplateFile} --parameters {ParameterFile}')
+ParameterFile = '@ProdEnvTemplates/ProdVM2Parameters.json'
+os.system(f'az deployment group create --name {TemplateName} --resource-group {ResourceGroup} --template-file {TemplateFile} --parameters {ParameterFile}')
 
 
- 
-#Creating the production environment
-
-# ResourceGroup = 'ProductionRG'
-# TemplateName = 'ProductionTemplate'
-
-# TemplateFile = 'templates/azuredeploy.json'
-# ParameterFile = '@templates/vm_parameters.json'
-
-# os.system(f'az group create --name {ResourceGroup} --location {Location}')
-# os.system(f'az deployment group create --name {TemplateName} --resource-group {ResourceGroup} --template-file {TemplateFile} --parameters {ParameterFile}')
+# Deploying the production database
+TemplateFile = 'ProdEnvTemplates/DeployProdDB.json'
+ParameterFile = '@ProdEnvTemplates/ProdDBParameters.json'
+os.system(f'az deployment group create --name {TemplateName} --resource-group {ResourceGroup} --template-file {TemplateFile} --parameters {ParameterFile}')
