06597c0252f9896096fef222e63959d8dbd8ca10
jcric <jonas.c.richter@me.com>
2022-05-20 11:30:56 +0200

    Finalized functionality

--- ResizeVM.py
+++ ResizeVM.py
@@ -2,15 +2,13 @@ from multiprocessing.sharedctypes import SynchronizedString
 import os
 
 SubscriptionId = '07c29cd9-f0b0-42f7-be1c-01d2cfc5ee2c'
-ResourceGroup = ''
-VirtualMachine = ''
+ResourceGroup = 'FortmuAppTeamTest'
+VirtualMachine = 'FortmuTestVM'
 Size = ''
-
-# Run in terminal to list available Virtual Machine sizes:
-# os.system(f'az vm list-vm-resize-options {ResourceGroup} --name {VirtualMachine} --output table')
 
 os.system('az login')
 os.system(f'az account set --subscription {SubscriptionId}')
-# os.system(f'az vm resize --resource-group {ResourceGroup} --name {VirtualMachine} --size {Size}')
+os.system(f'az vm list-vm-resize-options --resource-group {ResourceGroup} --name {VirtualMachine} --output table')
 
-os.system('az group list --output table')
+Size = input("Please enter the VM-size to update to.")
+os.system(f'az vm resize --resource-group {ResourceGroup} --name {VirtualMachine} --size {Size}')
